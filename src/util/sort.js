9a43a5d5c29753b2ba92d6b4be61be3dd5ec0068
marypanasenko <kerch.pm@gmail.com>
2021-12-24 11:31:24 +0300

    add const esc, escape to individual file enum.js

--- src/util/sort.js
+++ src/util/sort.js
@@ -1,10 +1,8 @@
-
 export const sortByDate = (b, a) => {
   if(a.film.year !== b.film.year){
     return a.film.year - b.film.year;
   }
 };
-
 
 export const sortByRating = (b, a) => {
   if(a.film.rating !== b.film.rating){
